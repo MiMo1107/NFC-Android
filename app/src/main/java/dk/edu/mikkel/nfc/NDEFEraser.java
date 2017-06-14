40117d05281675d0943756a118843d908519112a
MiMo1107 <mikkel1107@gmail.com>
2017-06-14 21:49:18 +0200

    NDEF format, delete and cleaning

--- app/src/main/java/dk/edu/mikkel/nfc/NDEFEraser.java
+++ app/src/main/java/dk/edu/mikkel/nfc/NDEFEraser.java
@@ -5,6 +5,9 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.nfc.FormatException;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -16,6 +19,10 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -26,6 +33,12 @@ public class NDEFEraser extends AppCompatActivity {
     private NfcAdapter mAdapter;
     private AlertDialog mDialog;
     private PendingIntent mPendingIntent;
+    private Button cleanButton;
+    private Button addButton;
+    private Button deleteButton;
+    private TextView formatText;
+    private TextView ndefText;
+    private Tag myTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +55,12 @@ public class NDEFEraser extends AppCompatActivity {
         }
         mPendingIntent = PendingIntent.getActivity(this, 0,
                 new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        cleanButton = (Button) (findViewById(R.id.erase_cleanButton));
+        addButton = (Button) (findViewById(R.id.erase_addButton));
+        deleteButton = (Button) (findViewById(R.id.erase_deleteButton));
+        ndefText = (TextView) (findViewById(R.id.erase_ndefFormated));
+        formatText = (TextView) (findViewById(R.id.erase_ndefNotFormated));
     }
 
     private void showMessage(String title, String message) {
@@ -57,28 +76,60 @@ public class NDEFEraser extends AppCompatActivity {
                 || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
             Vibrator v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
             v.vibrate(100);
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            if(NdefFormatable.get(tag) != null){
-                try {
-                    NdefRecord[] records = new NdefRecord[]{ createRecord("") };
-                    NdefMessage message = new NdefMessage(records);
-                    formatTag(NdefFormatable.get(tag),message);
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-
-            } else if( Ndef.get(tag) != null){
-                try {
-                    NdefRecord[] records = new NdefRecord[]{ createRecord("") };
-                    NdefMessage message = new NdefMessage(records);
-                    cleanTag(Ndef.get(tag), message);
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
+            myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            if(NdefFormatable.get(myTag ) != null){
+                    addButton.setAlpha(1);
+                    addButton.setClickable(true);
+                    formatText.setTextColor(Color.parseColor("#000000"));
+                deleteButton.setAlpha(0.5f);
+                deleteButton.setClickable(false);
+                cleanButton.setAlpha(0.5f);
+                cleanButton.setClickable(false);
+                ndefText.setTextColor(Color.parseColor("#808080"));
+            } else if( Ndef.get(myTag ) != null){
+                addButton.setAlpha(0.5f);
+                addButton.setClickable(false);
+                formatText.setTextColor(Color.parseColor("#808080"));
+                    deleteButton.setAlpha(1);
+                    deleteButton.setClickable(true);
+                    cleanButton.setAlpha(1);
+                    cleanButton.setClickable(true);
+                    ndefText.setTextColor(Color.parseColor("#000000"));
             } else {
                 Toast.makeText(this, "No NDEF support", Toast.LENGTH_SHORT).show();
+                addButton.setAlpha(0.5f);
+                addButton.setClickable(false);
+                formatText.setTextColor(Color.parseColor("#808080"));
+                deleteButton.setAlpha(0.5f);
+                deleteButton.setClickable(false);
+                cleanButton.setAlpha(0.5f);
+                cleanButton.setClickable(false);
+                ndefText.setTextColor(Color.parseColor("#808080"));
             }
         }
+    }
+    public void cleanClick(View view){
+        try {
+            NdefRecord[] records = new NdefRecord[]{ createRecord("") };
+            NdefMessage message = new NdefMessage(records);
+            cleanTag(Ndef.get(myTag), message);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addClick(View view){
+        try {
+            NdefRecord[] records = new NdefRecord[]{ createRecord("") };
+            NdefMessage message = new NdefMessage(records);
+            formatTag(NdefFormatable.get(myTag), message);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteClick(View view){
+            deleteFormating(Ndef.get(myTag));
     }
 
     private void cleanTag(Ndef ndef, NdefMessage msg){
@@ -130,6 +181,24 @@ public class NDEFEraser extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
+    }
+
+    private void deleteFormating (Ndef ndefTag) {
+        if(ndefTag != null){
+            try {
+                ndefTag.connect();
+                ndefTag.writeNdefMessage(new NdefMessage(new NdefRecord(NdefRecord.TNF_EMPTY, null, null, null)));
+                ndefTag.close();
+                Toast.makeText(this, "Tag formatting deleted", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (FormatException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(this, "Lost connection to the tag", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private NdefRecord createRecord(String text) throws UnsupportedEncodingException {
@@ -194,4 +263,24 @@ public class NDEFEraser extends AppCompatActivity {
         return;
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        // TODO Auto-generated method stub
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            ImageView img = (ImageView)findViewById(R.id.erase_searching);
+            AnimationDrawable frameAnimation = (AnimationDrawable) img.getDrawable();
+            frameAnimation.start();
+        }
+    }
+
+    public void clickTag(View view){
+        ImageView img = (ImageView)findViewById(R.id.erase_searching);
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getDrawable();
+        if(frameAnimation.isRunning()){
+            frameAnimation.stop();
+        } else {
+            frameAnimation.start();
+        }
+    }
 }
